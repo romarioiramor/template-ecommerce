bffe8a7966fa34630d0358672dadd1acb2fa0e45
oiramoromario <romario.adsistemas@gmail.com>
2021-06-29 20:38:45 -0300

    modelando template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexComponent } from './index/index.component';
+import { CartComponent } from './cart/cart.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { ShopComponent } from './shop/shop.component';
+import { SingleProductComponent } from './single-product/single-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexComponent,
+    CartComponent,
+    CheckoutComponent,
+    ShopComponent,
+    SingleProductComponent
   ],
   imports: [
     BrowserModule,
